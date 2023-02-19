452c8da6b656b6e475af15d04469582db06c3aad
victorstdev <victorstdev@gmail.com>
2023-02-19 19:45:25 -0300

    Add polls app and template views

---
+++ polls_project/polls/views.py
@@ -0,0 +1,40 @@
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse, Http404, HttpResponseRedirect
+from django.template import loader
+from django.urls import reverse
+from django.views.generic import TemplateView, DetailView, ListView
+from .models import *
+# Create your views here.
+
+class IndexView(ListView):
+    template_name = "polls/index.html"
+    context_object_name = 'latest_question_list'
+    def get_queryset(self):
+        return Question.objects.order_by('-pub_date')[:5]    
+
+class QuestionDetailView(DetailView):
+    model = Question
+    template_name = "polls/detail.html"
+
+class ResultsDetailView(DetailView):
+    model = Question
+    template_name = "polls/results.html"
+
+
+def vote(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        return render(
+            request,
+            'polls/detail.html',
+            {
+                'question': question,
+                'error_message': 'Ei, você não selecionou uma opção.'
+            }
+        )
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        return HttpResponseRedirect(reverse('polls:results', args=(question_id,)))
